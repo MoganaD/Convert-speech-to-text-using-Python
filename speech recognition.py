d524754e8b4e531d1e692415011ccb87ddbc9d33
Mogana Darshini Ganggayah <42921710+MoganaD@users.noreply.github.com>
2022-04-28 21:07:57 +0800

    Update speech recognition.py

--- speech recognition.py
+++ speech recognition.py
@@ -9,33 +9,33 @@ pip install SpeechRecognition moviepy
 
 # In[3]:
 
-
+#import modules
 import speech_recognition as sr 
 import moviepy.editor as mp
 
 
 # In[70]:
 
-
-clip = mp.VideoFileClip(r"Documents/yoges/video5.mp4")
+#load video clip
+clip = mp.VideoFileClip("Documents/video5.mp4")
 
 
 # In[71]:
 
-
-clip.audio.write_audiofile(r"Documents/yoges/video5.wav")
+#convert video to audio
+clip.audio.write_audiofile("Documents/video5.wav")
 
 
 # In[72]:
 
-
+#use recognizer to extract the audio
 r = sr.Recognizer()
 
 
 # In[73]:
 
 
-audio = sr.AudioFile("Documents/yoges/video5.wav")
+audio = sr.AudioFile("Documents/video5.wav")
 
 
 # In[77]:
@@ -48,15 +48,15 @@ result = r.recognize_google(audio_file)
 
 # In[76]:
 
-
+#display the text from the audio
 result
 
 
 # In[79]:
 
-
+#export the text
 # exporting the result 
-with open('Documents/yoges/text5.txt',mode ='w') as file: 
+with open('Documents/text5.txt',mode ='w') as file: 
    file.write("Recognized Speech:") 
    file.write("\n") 
    file.write(result) 
